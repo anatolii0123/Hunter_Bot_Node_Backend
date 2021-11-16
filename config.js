73d6da7d58127297a1bccd45b8297ce1ed9e8e70
Hunter727 <globalhunter727@gmail.com>
2021-11-17 05:55:43 +0900

    init repo

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    token: {
+        iv: 'd5cf7b01c40e2b6df0b5d206e2c8ac99',
+        content: '656e42f523d9b2099ab3bb2a1884942fcb8b87f89bb01a712d79e1a4ff52c23a2b495f2fbc56d300f41405c3155f62279afb2fb9f0b3055014b172'
+    }, // crypto token from bot
+
+    prefix: "v-", // default prefix
+    database: "mongodb+srv://guardian:ASDFasdf1234@cluster0.i86ba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // link from mongodb
+    clientId: "910229962349486082", //client id from bot
+    clientSecret: "Ra235S8MRnjqrTwDe5X-2gxm99IKSOlb", // client secret from bot
+    callbackurl: "/api/auth/discord/redirect", // callback
+    port: 8000, // port
+    FRONTEND_URL: "https://hunter-bot-backend.herokuapp.com" // Front-End URL
+}
