f0f0f710b1782d3fc6f8fec720fdbbb552c72cfb
SmartDev84 <smartdev@innovdevteam.com>
2021-11-19 19:10:33 +0900

    fix

--- routers/api.js
+++ routers/api.js
@@ -2,8 +2,11 @@ const app = require('express').Router()
 
 const authentification = require('./api/auth')
 const discord = require('./api/discord')
+const discord = require('./api/role')
 
 app.use('/auth', authentification)
 app.use('/discord', discord)
+app.use('/role', role)
+
 
 module.exports = app;
