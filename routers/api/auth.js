999c1137777fe8f870e054d84a33b3f01597cd78
Hunter727 <globalhunter727@gmail.com>
2021-11-21 03:06:36 +0900

    Update auth.js

--- routers/api/auth.js
+++ routers/api/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/authController');
+const passport = require('passport')
 
-router.get('/', controller.get);
+router.get('/', passport.authenticate('discord'));
 router.get('/discord', controller.discord);
 router.get('/discord/redirect', controller.redirect);
 
