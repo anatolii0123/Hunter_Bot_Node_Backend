cb4d0c9df78b4187ea70c29697f8dcb00e290fef
Hunter727 <globalhunter727@gmail.com>
2021-11-21 07:24:01 +0900

    Update index.js

--- services/roleService/index.js
+++ services/roleService/index.js
@@ -5,7 +5,7 @@ const crypto = require("../../utils/crypto")
 
 const getRoles = async (guildId) => {
     console.log("id", guildId)
-    const result = await fetch(`http://discord.com/api/v9/guilds/${guildId}/roles`, {
+    const res = await fetch(`http://discord.com/api/v9/guilds/${guildId}/roles`, {
         method: "GET",
         headers: {
             Authorization: `Bot ${crypto.decrypt(config.token)}`,
@@ -13,7 +13,7 @@ const getRoles = async (guildId) => {
     });
 
     return {
-        roles: await result.json()
+        roles: res.json()
     }
 }
 
